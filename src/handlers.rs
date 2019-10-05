5b1debc027e77e66ebe6db753cd0d1cf008ff9a7
Dwi Sulfahnur <hello@dwisulfahnur.com>
2019-10-05 16:52:03 +0700

    Update handlers file

--- src/handlers.rs
+++ src/handlers.rs
@@ -5,15 +5,19 @@ use futures::Future;
 use juniper::http::graphiql::graphiql_source;
 use juniper::http::GraphQLRequest;
 
-use crate::graphql::{Context, Mutation, Query, Schema};
+use crate::db::{Pool, get_db_pool};
+use crate::schemas::{Context, Schema};
+use crate::schemas::create_schema;
 
 pub fn graphql(
+    pool: web::Data<Pool>,
     schema: web::Data<Arc<Schema>>,
     data: web::Json<GraphQLRequest>,
 ) -> impl Future<Item=HttpResponse, Error=Error> {
-    let ctx = Context {};
+    let ctx = Context {
+        dbpool: pool.get_ref().to_owned(),
+    };
     web::block(move || {
-        let res = data.execute(&schema, &ctx);
         let res = data.execute(&schema, &ctx);
         Ok::<_, serde_json::error::Error>(serde_json::to_string(&res)?)
     })
@@ -28,13 +32,12 @@ pub fn graphql(
 pub fn graphql_playground() -> HttpResponse {
     HttpResponse::Ok()
         .content_type("text/html; charset=utf-8")
-        .body(graphiql_source("/graphql/"))
+        .body(graphiql_source("/graphql"))
 }
 
 
 pub fn register(config: &mut web::ServiceConfig) {
-    let schema = std::sync::Arc::new(Schema::new(Query, Mutation));
-
+    let schema = std::sync::Arc::new(create_schema());
     config
         .data(schema)
         .route("/graphql", web::post().to_async(graphql))
