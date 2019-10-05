b773778c40c8599144007b4eb676b3e200d2dc5b
Dwi Sulfahnur <hello@dwisulfahnur.com>
2019-10-05 19:05:01 +0700

    add user field to the product object, it will be returning user object.

--- src/schemas/product.rs
+++ src/schemas/product.rs
@@ -2,19 +2,52 @@ use juniper::{FieldError, FieldResult, RootNode};
 use juniper;
 use mysql::{Error as DBError, from_row, params, Row};
 
-#[derive(Default, Debug, GraphQLObject)]
-#[graphql(description = "Product")]
+use crate::schemas::root::Context;
+use crate::schemas::user::User;
+
+/// Product
+#[derive(Default, Debug)]
 pub struct Product {
-    id: String,
-    user_id: String,
-    name: String,
-    price: f64,
+    pub id: String,
+    pub user_id: String,
+    pub name: String,
+    pub price: f64,
+}
+
+#[juniper::object(Context = Context)]
+impl Product {
+    fn id(&self) -> &str {
+        &self.id
+    }
+    fn user_id(&self) -> &str {
+        &self.user_id
+    }
+    fn name(&self) -> &str {
+        &self.name
+    }
+    fn price(&self) -> f64 {
+        self.price
+    }
+
+    fn user(&self, context: &Context) -> Option<User> {
+        let mut conn = context.dbpool.get().unwrap();
+        let user: Result<Option<Row>, DBError> = conn.first_exec(
+            "SELECT * FROM user WHERE id=:id",
+            params! {"id" => &self.user_id},
+        );
+        if let Err(err) = user {
+            None
+        }else{
+            let (id, name, email) = from_row(user.unwrap().unwrap());
+            Some(User { id, name, email })
+        }
+    }
 }
 
 #[derive(GraphQLInputObject)]
 #[graphql(description = "Product Input")]
 pub struct ProductInput {
-    user_id: String,
-    name: String,
-    price: f64,
+    pub user_id: String,
+    pub name: String,
+    pub price: f64,
 }
